af7db296a46cf7d0dd310cf679288c1d0c5c969d
gdgski <gregorio.degiuli2@gmail.com>
2025-02-20 12:26:20 +0100

    creazione pseudocodice
    creazione pseudocodice

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-#ciao, non so cosa scrivere :)
+#Pseudocodice
+# creo una lista di variabili possibili
+# inserimento da parte del utente della sua scelta
+# scelta computer e uguale a una scelta randomica della lista delle variabili possibili
+# scelta utente uguale la sua scelta
+# tutte le if
+#     if scelta utente == forbice:
+#             if scelta_computer = sasso:
+                    
